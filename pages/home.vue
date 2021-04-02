b5df20352a8c08040cc55651f76250d011cf6891
gagany <gagandaroach@gmail.com>
2021-04-02 17:29:08 -0500

    add layouts

---
+++ pages/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">DAROACHNET HOME PAGE</h1>
+      <NuxtLink to="/"> Back to Landing </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default {
+  layout: "default",
+};
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,28 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        DAROACHNET
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div
+    class="p-6 max-w-lg mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4"
+  >
+    <Logo />
+    <div class="flex flex-row">
+    <h1 class="text-lg">DAROACHNET</h1>
+    </div>
+    <div class="flex flex-row">
+      <NuxtLink
+        to="/home"
+        class="flex w-auto bg-red-600 rounded-md px-4 py-2 text-white hover:bg-white hover:border-red-600 hover:border-2 hover:text-red-600"
+      >
+        Enter Page
+      </NuxtLink>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
-export default Vue.extend({})
+export default Vue.extend({
+  layout: "landing",
+});
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
