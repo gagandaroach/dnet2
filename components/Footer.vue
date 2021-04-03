d3a3c7ca7c7b174185c7be76dfb779d1464c4aa5
gagan <gagandaroach@gmail.com>
2021-04-02 19:57:08 -0500

    align footer to bottom

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,42 @@
 <template>
-  <div class="">
-      Footer
+  <div>
+    <footer class="h-auto bg-gray-200">
+      <div class="flex flex-row">
+        <div class="flex flex-col ml-1 mr-auto">
+          <span
+            ><a
+              :title="source_code_popup_text"
+              href="https://github.com/gagandaroach/daroachnet"
+              >{{ source_code_link_title }}</a
+            ></span
+          >
+        </div>
+        <div class="flex flex-col mr-1 ml-auto">
+          <a
+            :title="copyright_popup_text"
+            href="mailto:gagandaroach@gmail.com?Subject=Hello Gagan, I like daroach.net!"
+            target="_top"
+            ><span>Gagan Daroach &copy; 2019-2021</span></a
+          >
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+  data() {
+    return {
+      source_code_popup_text:
+        "Complete full stack code example for this website are available on Github. Also see the complete development history of this website, one changed line at a time.",
+      source_code_link_title: "Website Source Code",
+      copyright_popup_text: "Questions, Comments, or Concerns? Email me :).",
+    };
+  },
+});
 </script>
 
 <style></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="h-screen bg-red-900">
-    <Navbar />
-    <Nuxt />
-    <Footer />
+    <div class="h-10">
+      <Navbar />
+    </div>
+    <div class="m-auto">
+      <Nuxt />
+    </div>
+    <div class="app-footer">
+      <Footer />
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.app-footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+</style>
