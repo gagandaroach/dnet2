5422574c87cff25182aa38773dfff87dfccec849
gagan <gagandaroach@gmail.com>
2021-04-02 19:29:11 -0500

    create main centered landing card

---
+++ components/Landing.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex h-screen">
+    <div
+      class="p-10 m-auto max-w-lg bg-gray-200 rounded-xl shadow-md text-center"
+    >
+      <Logo />
+      <div class="my-4">
+        <h1 class="text-lg">Daroach.net</h1>
+        <h1 class="text-base">Gagan's Web Server</h1>
+      </div>
+      <div class="mt-3">
+        <NuxtLink
+          to="/home"
+          class="w-52 bg-black rounded-lg px-4 py-2 text-white hover:bg-white hover:border-red-900 hover:border-2 hover:text-red-900"
+        >
+          Enter Page
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  layout: "landing",
+});
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,15 @@
 <template>
-  <div
-    class="p-6 max-w-lg mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4"
-  >
-    <Logo />
-    <div class="flex flex-row">
-    <h1 class="text-lg">DAROACHNET</h1>
-    </div>
-    <div class="flex flex-row">
-      <NuxtLink
-        to="/home"
-        class="flex w-auto bg-red-600 rounded-md px-4 py-2 text-white hover:bg-white hover:border-red-600 hover:border-2 hover:text-red-600"
-      >
-        Enter Page
-      </NuxtLink>
-    </div>
+  <div>
+    <Landing />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import landing from "~/layouts/landing.vue";
 
 export default Vue.extend({
+  components: { landing },
   layout: "landing",
 });
 </script>
